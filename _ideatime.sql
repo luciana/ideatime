c49d5ced2d1b5078cffbdd9bfb9dd6b682908e22
Mike Sabo <mike.sabo614@gmail.com>
2012-09-05 00:41:00 -0400

    checking in updated database. now has 8 tables with relationships.

--- _ideatime.sql
+++ _ideatime.sql
@@ -3,15 +3,41 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 30, 2012 at 11:45 PM
+-- Generation Time: Sep 05, 2012 at 12:39 AM
 -- Server version: 5.5.9
 -- PHP Version: 5.3.5
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
 --
--- Database: `_ideatime`
+-- Database: `_ideatime_`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `body` text NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `ideas_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_comments_ideas1` (`ideas_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `comments`
+--
+
 
 -- --------------------------------------------------------
 
@@ -22,6 +48,7 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 CREATE TABLE `groups` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
@@ -29,8 +56,27 @@ CREATE TABLE `groups` (
 -- Dumping data for table `groups`
 --
 
-INSERT INTO `groups` VALUES(1, 'The Founders Club');
-INSERT INTO `groups` VALUES(2, 'test group 2');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `group_access`
+--
+
+CREATE TABLE `group_access` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `groups_id` int(10) unsigned NOT NULL,
+  `users_whitelist_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`,`groups_id`),
+  KEY `fk_group_access_groups1` (`groups_id`),
+  KEY `fk_group_access_users_whitelist1` (`users_whitelist_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `group_access`
+--
+
 
 -- --------------------------------------------------------
 
@@ -41,18 +87,41 @@ INSERT INTO `groups` VALUES(2, 'test group 2');
 CREATE TABLE `ideas` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
-  `good` int(2) DEFAULT '0',
+  `good` int(2) NOT NULL DEFAULT '0',
   `bad` int(2) NOT NULL DEFAULT '0',
   `author` varchar(80) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=152 ;
+  `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `users_whitelist_id` int(10) unsigned NOT NULL,
+  `groups_id` int(10) unsigned NOT NULL,
+  `status_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_ideas_users_whitelist1` (`users_whitelist_id`),
+  KEY `fk_ideas_groups1` (`groups_id`),
+  KEY `fk_ideas_status1` (`status_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=182 ;
 
 --
 -- Dumping data for table `ideas`
 --
 
-INSERT INTO `ideas` VALUES(15, 'Marriage Insurance', 6, 0, 'skoscho');
-INSERT INTO `ideas` VALUES(2, 'Keep My Change', 1, 0, 'luciana123_2002');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `status`
+--
+
+CREATE TABLE `status` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(30) NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `status`
+--
+
 
 -- --------------------------------------------------------
 
@@ -67,27 +136,13 @@ CREATE TABLE `users` (
   `oauth_token` text,
   `oauth_secret` text,
   `username` text,
-  `group_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `group_id` (`group_id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=19 ;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` VALUES(7, 'twitter', '39792346', '39792346-w053ALPlJ3vddwja2mYCNWR58UjcLK6GLeJn3eOlg', 'pi9AVldkXsTznTfptlGMRAFrCuSLXhRL2hyZfpNtOV4', 'luciana123_2002', 1);
-INSERT INTO `users` VALUES(8, 'twitter', '320236200', '320236200-efD0A9naQ4vj3pGIrPRda0UOht2ed0RLpH87waAC', 'gSRRtMulGDH9iGxaZX8eHFwjmCjZHEpeQnDYFaQKc', 'MyDomainList', 1);
-INSERT INTO `users` VALUES(9, 'twitter', '17811921', '17811921-ndK3DgbEnYWUy6YJpqEKMCmwbpJesAqtQ4UPvigOA', 'mRomOynPucERxTBGexfXdXv3WtGXgzEhDsVTeJ4', 'DailyPush', 1);
-INSERT INTO `users` VALUES(10, 'twitter', '5940422', '5940422-OtnUSDTrEUSSzRemAhfnDXUy9MipOklHT3ACl89ihI', 'MSfF5iT3WCDdmuSvCNpuLdAMitrWWRQ2hV6Uun4', 'clevelandrachel', 1);
-INSERT INTO `users` VALUES(11, 'twitter', '613758825', '613758825-RtjUgoeOguzuVMowl99I9idfF1eukmphO0qb389A', '7Ztv9bQXnqbddgPu8if0yrWOgvigeu3LTBretE', 'JeneticCode', 1);
-INSERT INTO `users` VALUES(12, 'twitter', '434626592', '434626592-R4bRlzXUN4gcr03yb3fD2w4PeKmuwOg6AfrvNit5', 'L4j9GWakPnUIdGT8q0maSaw62LEiVqFrPpQXOqmL0g', 'getSabotaged', 1);
-INSERT INTO `users` VALUES(13, 'twitter', '109142418', '109142418-oveyBGxtiiWkWAabLNT48EMA1tUSQ41uC5gevkM', 'huvlfbaIP85AlRhHCCnHOlqhb8XXBJTWl6WYaqI9M', 'skoscho', 1);
-INSERT INTO `users` VALUES(14, 'twitter', '54058772', '54058772-VecXAJorUyaaAH4yjz5CMiazEgxfVDPq0rUY6C6TE', 'PD2SiYAPCFRJ83WdThZt6qvYa18vacStCL5up2U90', 'MightyAnger', 1);
-INSERT INTO `users` VALUES(15, 'twitter', '51091005', '51091005-QSR1TKHCnH1coR4OrHdrtlujXKw4ydmnM7PzEfYZ5', 'ygNUMiQJ0aDs701VJo2luuPZ7Q6M9yYp7OUjr0JY', 'tjhadley15', 1);
-INSERT INTO `users` VALUES(16, 'twitter', '18357809', '18357809-uD0YAJadH1DJ4188a5oX6yhzLGxLhsqdc44wOfBqi', 'prVIcktbhDs95tiOMmyylXE2wATMXiAx14cKCZkqY', 'bsurtz', 1);
-INSERT INTO `users` VALUES(17, 'twitter', '17476319', '17476319-h8VUjxoF5v2kQhRJbM8S13sEh1LXf8goepTpBLHps', 'Re3YUfOKyichaFFBmRUhKIXlXaPHoiw4qU8HrPP8', 'jessicaenglund', 1);
-INSERT INTO `users` VALUES(18, 'twitter', '755117858', '755117858-mxiV2wgwFstEK0yAiI9a2mjuQabh7mowCOrzPToG', 'dh42G2hlM3ZTYWjO1MU3Bg3bgWLH552skqbMvA2w', 'JPierceCU', 1);
 
 -- --------------------------------------------------------
 
@@ -98,24 +153,58 @@ INSERT INTO `users` VALUES(18, 'twitter', '755117858', '755117858-mxiV2wgwFstEK0
 CREATE TABLE `users_whitelist` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `username` text,
-  PRIMARY KEY (`id`)
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `users_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_users_whitelist_users` (`users_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
 
 --
 -- Dumping data for table `users_whitelist`
 --
 
-INSERT INTO `users_whitelist` VALUES(1, 'luciana123_2002');
-INSERT INTO `users_whitelist` VALUES(2, 'MightyAnger');
-INSERT INTO `users_whitelist` VALUES(3, 'pawanpoudel');
-INSERT INTO `users_whitelist` VALUES(4, 'TheeRealDG');
-INSERT INTO `users_whitelist` VALUES(5, 'vladtoader');
-INSERT INTO `users_whitelist` VALUES(6, 'bradphilips');
-INSERT INTO `users_whitelist` VALUES(7, 'JeneticCode');
-INSERT INTO `users_whitelist` VALUES(8, 'clevelandrachel');
-INSERT INTO `users_whitelist` VALUES(9, 'dailypush');
-INSERT INTO `users_whitelist` VALUES(10, 'obas614');
-INSERT INTO `users_whitelist` VALUES(11, 'skoscho');
-INSERT INTO `users_whitelist` VALUES(12, 'tjhadley15');
-INSERT INTO `users_whitelist` VALUES(13, 'astroteg');
-INSERT INTO `users_whitelist` VALUES(14, 'bsurtz');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `votes`
+--
+
+CREATE TABLE `votes` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `author` varchar(50) NOT NULL,
+  `good` int(10) unsigned NOT NULL,
+  `bad` int(10) unsigned NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `ideas_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_votes_ideas1` (`ideas_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `votes`
+--
+
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `fk_comments_ideas1` FOREIGN KEY (`ideas_id`) REFERENCES `ideas` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `group_access`
+--
+ALTER TABLE `group_access`
+  ADD CONSTRAINT `fk_group_access_groups1` FOREIGN KEY (`groups_id`) REFERENCES `groups` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_group_access_users_whitelist1` FOREIGN KEY (`users_whitelist_id`) REFERENCES `users_whitelist` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `votes`
+--
+ALTER TABLE `votes`
+  ADD CONSTRAINT `fk_votes_ideas1` FOREIGN KEY (`ideas_id`) REFERENCES `ideas` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
