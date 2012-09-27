b89efbe02003bac5507557b04a9f710a43e08a50
Luciana Bruscino <luciana.bruscino@gmail.com>
2012-09-26 23:34:26 -0400

    added group home page

---
+++ application/views/groups/list_group_view.php
@@ -0,0 +1,22 @@
+<div class="row-fluid">  	
+		<div class="well">
+        <h3 class="pink">My Groups</h3> 
+		<div class="row-fluid">  
+        <?php
+		foreach ($groups as $group)
+		{ ?>
+		   <div class="well <?php echo $class ?>">
+		   	<div class="span3">
+		   		<img src="#" class="img-polaroid thumbnail">
+		   	</div>
+  			<div class="span9">		   	
+		    	<h3><a href="<?php echo site_url('ideas/home/'.$group->groups_id)?>" id="group-<?php echo $group->id ?>"><?php echo $group->name; ?> Group</a></h3>		    
+		    </div>
+		   </div>
+		   <?php
+		}
+		?>
+		</div>
+	</div>
+	</div>     
+</div>
