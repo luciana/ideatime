94fb628182c5800a6405683bb8299b6b3e3c095a
Luciana Bruscino <luciana.bruscino@gmail.com>
2013-03-23 09:46:36 -0400

    oscar banner

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -1,13 +1,16 @@
-
 <?php $this->load->view('common/header') ?>
 <section class="container-fluid">
      
        <?php if(empty($_SESSION['name'])){  ?>
         <div class="row-fluid">
           <div class="span12 tagline center well">
-            <a href=<?php echo '"' . site_url('ideas/login') . '" class="uppercase">'?> Sign in with your twitter account</a>                          
+            <a onclick="dataLayer.push({'event': 'GAEvent', 'eventCategory':window.location.href, 'eventAction':this.name, 'eventLabel':""});" href=<?php echo '"' . site_url('ideas/login') . '" class="uppercase" name="authentication" >'?> Sign in with your twitter account</a>                          
           </div>          
         </div>                 
         <?php } ?>   
 </section>
+<section class="center">
+	<img src="/img/ideatime_oscar_banner.png"  />
+</section>
+
 <?php $this->load->view('common/footer') ?>
