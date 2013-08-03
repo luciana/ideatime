11abbaf7e26df0b6b42081369ab917aa15c95472
luciana <luciana.bruscino@gmail.com>
2013-08-03 13:08:55 -0400

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'chad_ideatime';
-$db['default']['password'] = 'Pr0gressive';
-$db['default']['database'] = 'chad_ideatime';
+$db['default']['username'] = 'root';
+$db['default']['password'] = 'root';
+$db['default']['database'] = '_ideatime';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
